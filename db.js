8b6556c821124b3a38c9595cf39e83162ce361f1
krissy <kmujakic@gmail.com>
2023-10-04 11:04:37 +0200

    initialized project

---
+++ db.js
@@ -0,0 +1,20 @@
+/** Database config for database. */
+
+
+const { Client } = require("pg");
+const { DB_URI } = require("./config");
+
+const db = new Client(DB_URI);
+
+db.connect()
+  .then(() => {
+    console.log('Connected to the PostgreSQL database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error.message);
+  });
+
+db.connect();
+
+
+module.exports = db;
